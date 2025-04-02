305d180d9b63d430e9f2b879ec12582d778af96c
jmseb3 <jmseb3@naver.com>
2025-04-02 11:25:36 +0900

    chore(gradle): apply dokka

--- Vibrate/build.gradle.kts
+++ Vibrate/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.android.library)
-    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
+    alias(libs.plugins.maven)
+    alias(libs.plugins.dokka)
 }
 
 kotlin {
@@ -22,6 +25,12 @@ kotlin {
             implementation(kotlin("test"))
         }
 
+        iosMain.dependencies {
+
+        }
+        androidMain.dependencies {
+
+        }
     }
 
     //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
@@ -42,4 +51,33 @@ android {
     defaultConfig {
         minSdk = 21
     }
+}
+
+dokka {
+    moduleName.set("KMP-Vibrate")
+
+    dokkaPublications.html {
+        outputDirectory.set(project.mkdir("build/dokka"))
+    }
+
+    dokkaSourceSets {
+        this.commonMain {
+            displayName.set("Common")
+        }
+        named("androidMain") {
+            displayName.set("Android")
+        }
+        named("iosMain") {
+            displayName.set("iOS")
+        }
+    }
+}
+
+mavenPublishing {
+    configure(
+        KotlinMultiplatform(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
+            sourcesJar = true
+        )
+    )
 }
